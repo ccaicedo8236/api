d7939836f10ebe7bf03f55e6b76d3c661579ff9e
caicedocesar <caicedocesar@yahoo.com>
2022-02-17 11:40:18 -0500

    ultimos cambios

--- configuracion/conexion.js
+++ configuracion/conexion.js
@@ -1,5 +1,8 @@
+// Con el paquete MySQL instalado se procede a cargar la librería
 const mysql = require('mysql');
 
+// Se define la conexión con los parámetros necesarios como host,
+// usuario, clave, puerto y base de datos
 const conexion = mysql.createConnection(
     {host: 'localhost',
     user: 'api',
@@ -7,7 +10,9 @@ const conexion = mysql.createConnection(
     port: 3306,
     database: 'testAPI'
 }
-)
+);
+// Si la conexión es exitosa muestra un mensaje por consola indicándolo
+// caso contrario muestra el error generado
 conexion.connect((err)=>{
     if (err){
         console.log('error: '+err)}
@@ -17,4 +22,5 @@ conexion.connect((err)=>{
     }
 )
 
+// Se devuelve la conexión establecida
 module.exports=conexion;
